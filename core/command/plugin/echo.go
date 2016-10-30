715bec9566cbacdaba3f54d07b1fac923dd5bb63
K <x00.x7f@gmail.com>
2016-10-30 19:12:52 +0900

    Added description messages

--- core/command/plugin/echo.go
+++ core/command/plugin/echo.go
@@ -2,6 +2,10 @@ package plugin
 
 import "strings"
 
+// DetailEcho is description echo command
+var DetailEcho = "Returns a string that has been your input."
+
+// RunEcho is root function of echo command
 func RunEcho(args ...string) (string, error) {
-	return strings.Join(args, ", "), nil
+	return strings.Join(args, " "), nil
 }
--- core/command/plugin/ping.go
+++ core/command/plugin/ping.go
@@ -1,5 +1,8 @@
 package plugin
 
+// DetailPing is description ping command
+var DetailPing = "Return 'pong'."
+
 func RunPing(args ...string) (string, error) {
 	return "pong", nil
 }
